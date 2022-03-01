afcb2d301eca9a2b54c78c2dca91b2cddf23c9f1
Yusan Yin <yusanyin@gmail.com>
2022-02-28 17:01:09 -0800

    add fizzbuzz

---
+++ Web Fundamentals 2021/Fizzbuzz/script.js
@@ -0,0 +1,11 @@
+for (var x = 0; x <= 100; x++) {
+    if (x % 3 == 0 && x % 5 == 0) {
+        console.log("FizzBuzz")
+    } else if (x % 3 == 0) {
+        console.log("Fizz")
+    } else if (x % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(x);
+    }
+}
