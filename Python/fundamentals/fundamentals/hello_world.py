741604b422ea6f5c27019f02aeb14f0138567ceb
Yusan Yin <yusanyin@gmail.com>
2022-03-06 22:26:20 -0800

    add python hello world

---
+++ Python/fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+name = 'Yusan'
+print('Hello', name, '!')
+print('Hello ' + name + '!')
+num = 39
+print('Hello', num, '!')
+print('Hello ' + str(num) + '!')
+food_one = 'shrimp'
+food_two = 'pasta'
+print('I love to eat {} and {}!'.format(food_one, food_two))
+print(f'I love to eat {food_one} and {food_two}!')
