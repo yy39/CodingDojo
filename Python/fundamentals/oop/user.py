a24cd888d549456bca607eb35e3d91259a4f5367
Yusan Yin <yusanyin@gmail.com>
2022-03-09 01:39:59 -0800

    chain methods in python: user.py

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -25,24 +25,14 @@ user1 = User('Mr. Meow Meow', 7777777)
 user2 = User('Truck Freak',  2000)
 user3 = User('Yusan', -16000)
 
-user1.deposit(5000000)
-user1.deposit(50000000)
-user1.deposit(400000)
-user1.withdrawal(20000)
-user1.display_user_balance()
+user1.deposit(5000000).deposit(50000000).deposit(
+    400000).withdrawal(20000).display_user_balance()
 
-user2.deposit(1000)
-user2.deposit(2000)
-user2.withdrawal(50)
-user2.withdrawal(50)
-user2.display_user_balance()
+user2.deposit(1000).deposit(2000).withdrawal(
+    50).withdrawal(50).display_user_balance()
 
-user3.deposit(5)
-user3.withdrawal(100)
-user3.withdrawal(100)
-user3.withdrawal(100)
-user3.display_user_balance()
+user3.deposit(5).withdrawal(100).withdrawal(
+    100).withdrawal(100).display_user_balance()
 
-user1.transfer_money(user3, 1000000)
-user1.display_user_balance()
+user1.transfer_money(user3, 1000000).display_user_balance()
 user3.display_user_balance()
