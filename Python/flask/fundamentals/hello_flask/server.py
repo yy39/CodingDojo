f4286cc9fe8b976546d1d4ad47e1e61e4b273a4f
Yusan Yin <yusanyin@gmail.com>
2022-03-10 21:23:49 -0800

    pass list in hello flask

--- Python/flask/fundamentals/hello_flask/server.py
+++ Python/flask/fundamentals/hello_flask/server.py
@@ -23,6 +23,18 @@ def hello(banana, num):
     return render_template('hello.html', banana=banana, num=num)
 
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
+
+
 # app.run(debug=True) should be the very last statement!
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
