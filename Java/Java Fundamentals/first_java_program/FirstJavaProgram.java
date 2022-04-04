761a7cccad2b52faebc108204dd83f524cb28f8f
Yusan Yin <yusanyin@gmail.com>
2022-04-03 21:06:01 -0700

    Add Java

---
+++ Java/Java Fundamentals/first_java_program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My nbame is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
