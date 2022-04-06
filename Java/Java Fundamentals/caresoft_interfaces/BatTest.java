ea8af72c7031af10a1385b5ede4f174342675e5b
Yusan Yin <yusanyin@gmail.com>
2022-04-06 15:47:34 -0700

    add caresoft_interfaces

---
+++ Java/Java Fundamentals/caresoft_interfaces/BatTest.java
@@ -0,0 +1,16 @@
+
+public class BatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bat batInstance = new Bat();
+		batInstance.attackTown();
+		batInstance.attackTown();
+		batInstance.attackTown();
+		batInstance.eatHumans();
+		batInstance.eatHumans();
+		batInstance.fly();
+		batInstance.fly();
+	}
+
+}
---
+++ Java/Java Fundamentals/caresoft_interfaces/GorillaTest.java
@@ -0,0 +1,15 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla george = new Gorilla();
+		george.throwThings();
+		george.throwThings();
+		george.throwThings();
+		george.eatBananas();
+		george.eatBananas();
+		george.climb();
+	}
+
+}
