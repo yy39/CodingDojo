ce92e3ad98251b8400853942a2c4ee889281b476
Yusan Yin <yusanyin@gmail.com>
2022-05-03 16:54:09 -0700

    finish itstimetoduel

--- MERN/javascript/itstimetoduel.js
+++ MERN/javascript/itstimetoduel.js
@@ -4,7 +4,7 @@ class Card {
         this.cost = cost;
     }
 
-    isUnit(this) {
+    isUnit(target) {
         if (this instanceof Unit) {
             return true;
         } else {
@@ -21,29 +21,41 @@ class Unit extends Card {
     }
 
     attack(target) {
-        if (target.isUnit()) {
+        if (target.isUnit(target)) {
             target.resilience -= this.power;
         }
     }
 }
 
 class Effect extends Card {
-    constructor(name, cost, stat, mag) {
+    constructor(name, cost, text, stat, mag) {
         super(name, cost);
+        this.text = text;
         this.stat = stat;
         this.mag = mag;
     }
 
     play(target) {
-        if (target.isUnit()) {
+        if (target.isUnit(target)) {
             switch (this.stat) {
-                case "Resilience":
+                case "resilience":
                     target.resilience += this.mag;
-                case "Power":
+                    break;
+                case "power":
                     target.power += this.mag;
+                    break;
             }
         }
     }
 }
 
-const RedBeltNinja = new Unit("Red Belt Ninja", 3, 3, 4);
+const redBeltNinja = new Unit("Red Belt Ninja", 3, 3, 4);
+const hardAlgorithm = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", 3);
+hardAlgorithm.play(redBeltNinja);
+const blackBeltNinja = new Unit("Black Belt Ninja", 4, 5, 4);
+const unhandledPromiseRejection = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
+unhandledPromiseRejection.play(redBeltNinja);
+const pairProgramming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", 2);
+pairProgramming.play(redBeltNinja);
+redBeltNinja.attack(blackBeltNinja);
+console.log(blackBeltNinja);
