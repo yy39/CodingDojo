1116f9c047f4bd225503e37c883c7cd0713c6b83
Yusan Yin <yusanyin@gmail.com>
2022-05-04 14:30:37 -0700

    add bday button

--- MERN/javascript/test/test-react-app/src/components/PersonCard.jsx
+++ MERN/javascript/test/test-react-app/src/components/PersonCard.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+    }
     render() {
-        const { firstName, lastName, age, hairColor } = this.props
+        const { firstName, lastName, hairColor } = this.props
         return (
             <>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={() => this.setState({ age: this.state.age + 1 })}>Birthday Button for {firstName} {lastName}</button>
             </>
         )
     }
